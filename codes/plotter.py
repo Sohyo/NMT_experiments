0226baf3d42e6ced6df133a3efdf6e5e17620b8c
Sohyo <cienakim12@gmail.com>
2021-01-16 13:54:08 +0100

    plotter.py : fix bug

--- codes/plotter.py
+++ codes/plotter.py
@@ -19,28 +19,25 @@ def get_loss(file_path):
             # Get train loss
             if "| INFO | train |" in line:
                 # Extract loss by pattern "loss x.xxxx"
-                loss = re.search("loss \d{1,3}.\d{1,4}", line)
+                loss = re.search(r"loss \d{1,3}.\d{1,4}", line)
                 # Only get the digits from the extracted loss information
-                train_loss.append(re.sub("\Aloss\s", "", loss.group(0)))
+                train_loss.append(re.sub(r"\Aloss\s", "", loss.group(0)))
 
             # Get validation loss
             elif "| INFO | valid |" in line:
-                loss = re.search("loss \d{1,3}.\d{1,4}", line)
-                valid_loss.append(re.sub("\Aloss\s", "", loss.group(0)))
+                loss = re.search(r"loss \d{1,3}.\d{1,4}", line)
+                valid_loss.append(re.sub(r"\Aloss\s", "", loss.group(0)))
 
     return train_loss, valid_loss
 
 
 def get_BLEU(file_path):
-
     valid_BLEU = []
     with open(file_path) as f:
         for line in f:
             if "| INFO | valid |" in line:
-                print(line)
-                bleu = re.search("bleu \d{1,3}.\d{1,4}", line)
-                print(bleu)
-                valid_BLEU.append(re.sub("\Ableu\s", "", bleu.group(0)))
+                bleu = re.search(r"bleu \d{1,3}.\d{1,4}", line)
+                valid_BLEU.append(re.sub(r"\Ableu\s", "", bleu.group(0)))
 
     return valid_BLEU
 
@@ -54,12 +51,13 @@ def plot_loss(train_loss, valid_loss, filename):
     plt.show()
 
 
-def plot_BELU(valid_bleu,filename):
+def plot_BLEU(valid_bleu,filename):
     plt.plot(valid_bleu, label=f'Test loss {filename}')
     plt.ylabel('BLEU')
     plt.xlabel('Epochs')
     plt.legend(loc='lower right')
     plt.show()
+
 
 def main():
     file_path = "../slurms/slurm_JRC_p3"
@@ -67,7 +65,8 @@ def main():
     plot_loss(train_loss, valid_loss, "slurm_JRC_p3")
     valid_bleu = get_BLEU(file_path=file_path)
     print(valid_bleu)
-    plot_BELU(valid_bleu, filename="slurm_JRC_p3")
+    plot_BLEU(valid_bleu, filename="slurm_JRC_p3")
+
 
 if __name__ == '__main__':
     main()
