99492cb209875f8b2d431adf3ca0dc47b86dd814
Sohyo <cienakim12@gmail.com>
2021-02-08 23:41:01 +0100

    fixed wrong jobscripts for the non-tag phrases

---
+++ scripts/remove_unk_tag.py
@@ -0,0 +1,41 @@
+import os
+
+
+#
+# new_sys = []
+# with open(os.path.join(orig_dir, file_name)) as f:
+#     for line in f:
+#         if line.startswith('<unk> '):
+#             new_sys.append(line.split(' ', 1)[1])
+#         elif line.endswith('<unk>'):
+#             print(new_sys.append(line.split(' ', 1)[-1]))
+#         else:
+#             new_sys.append(line)
+# with open('../meaningful_results/g_EMEA_p_3.out.sys', 'w') as filehandle:
+#     for listitem in new_sys:
+#         filehandle.write('%s' % listitem)
+
+def arr2txt(arr, file_name):
+    with open(file_name, "w") as txt_file:
+        txt_file.writelines(arr)
+
+
+def remove_unk(orig_dir, file_name):
+
+    new_sys = []
+    with open(os.path.join(orig_dir, file_name)) as file:
+        for sentence in file:
+            if sentence.startswith('<unk> <unk> '):
+                new_sys.append(sentence.split(' ', 2)[2])
+            elif sentence.startswith('<unk> '):
+                new_sys.append(sentence.split(' ', 1)[1])
+            elif sentence.endswith('<unk>'):
+                new_sys.append(sentence.split(' ', 1)[-1])
+            else:
+                new_sys.append(sentence)
+    arr2txt(arr=new_sys, file_name=os.path.join(orig_dir, file_name))
+
+
+orig_dir = "../meaningful_results"
+file_name = "g_EMEA_p_4_old.out.sys"
+remove_unk(orig_dir=orig_dir, file_name=file_name)
